5b8dee124eebdf22a5814fe3351d3c4b1530844b
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-03-23 14:52:39 +0700

    update callApi function

--- src/utils/callApi.js
+++ src/utils/callApi.js
@@ -2,31 +2,22 @@ import { ANIME_BASE_URL, SERVER_BASE_URL } from "./constants"
 
 import axios from "axios"
 
-export const callAnimeApi = async ({
-  endpoint,
-  method = "GET",
-  payload = {}
-}) => {
-  return await axios({
-    method,
-    url: ANIME_BASE_URL + "/" + endpoint,
-    data: payload,
-    headers: {}
-  })
+const callApi = (base_url) => {
+  return async ({
+    endpoint,
+    method = "GET",
+    payload = {},
+    accessToken = null
+  }) =>
+    await axios({
+      method,
+      url: base_url + "/" + endpoint,
+      data: payload,
+      headers: {
+        Authorization: "Bearer " + accessToken
+      }
+    })
 }
 
-export const callServerApi = async ({
-  endpoint,
-  method = "GET",
-  payload = {},
-  accessToken = null
-}) => {
-  return await axios({
-    method,
-    url: SERVER_BASE_URL + "/" + endpoint,
-    data: payload,
-    headers: {
-      Authorization: "Bearer " + accessToken
-    }
-  })
-}
+export const callAnimeApi = callApi(ANIME_BASE_URL)
+export const callServerApi = callApi(SERVER_BASE_URL)
