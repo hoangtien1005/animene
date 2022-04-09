948d81e3d9c10ee025a1712422e92bd26f2c1e47
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-04-10 00:13:01 +0700

    add basic settings UI

--- src/containers/HomeTemplate/Settings/Settings.js
+++ src/containers/HomeTemplate/Settings/Settings.js
@@ -1,44 +1,126 @@
 import styles from "./styles.module.scss"
-import { useEffect, useState } from "react"
+import clsx from "clsx"
+import { useEffect, useState, useRef } from "react"
 import Editor from "../../../components/Editor"
+import Button from "../../../components/ui/Button"
+import AvatarPlaceholder from "../../../assets/img/avatar_placeholder.png"
 
 const Component = ({}) => {
-  const [editorState, setEditorState] = useState("")
+  const [about, setAbout] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [avatar, setAvatar] = useState()
+
+  const usernameRef = useRef()
+  const passwordRef = useRef()
+  const avatarRef = useRef()
 
   // TODO: set maximum characters
-  const handleEditorStateChange = (editorState) => {
-    setEditorState(editorState)
+  const handleAboutChange = (editorState) => {
+    setAbout(editorState)
   }
+
+  const handleUsernameChange = (e) => {
+    setUsername(usernameRef.current.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(passwordRef.current.value)
+  }
+
+  const handleAvatarChange = (e) => {
+    const file = avatarRef.current.files[0]
+    file.preview = URL.createObjectURL(file)
+    setAvatar(file)
+  }
+
+  useEffect(() => {
+    return () => {
+      avatar && URL.revokeObjectURL(avatar.preview)
+    }
+  }, [avatar])
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.section}>
-          <h4 className={styles.title}>About</h4>
-          <Editor
-            placeholder="A little about yourself..."
-            onStateChange={handleEditorStateChange}
-          />
-          <div className="space-top-40"></div>
-          <div dangerouslySetInnerHTML={{ __html: editorState }}></div>
-        </div>
-        <div className={styles.section}>
-          <h4 className={styles.title}>Avatar</h4>
+          <h4 className={styles.title}>Email</h4>
+          <input type="text" disabled value="Test@example" />
         </div>
         <div className={styles.section}>
           <h4 className={styles.title}>Username</h4>
+          <div>
+            <input
+              type="text"
+              onChange={handleUsernameChange}
+              value={username}
+              ref={usernameRef}
+            />
+          </div>
+          {username?.length > 0 && (
+            <Button className={styles.button}>Save</Button>
+          )}
         </div>
         <div className={styles.section}>
-          <h4 className={styles.title}>Email</h4>
+          <h4 className={styles.title}>About</h4>
+          <Editor
+            placeholder="A little about yourself..."
+            onStateChange={handleAboutChange}
+          />
+          {about?.length > 8 && <Button className={styles.button}>Save</Button>}
+        </div>
+        <div className={styles.section}>
+          <h4 className={styles.title}>Avatar</h4>
+          <div>
+            <div>
+              <input
+                type="file"
+                accept="image/*"
+                ref={avatarRef}
+                onChange={handleAvatarChange}
+              />
+            </div>
+            {avatar && (
+              <div
+                className={styles.avatar}
+                style={{ backgroundImage: `url(${avatar.preview})` }}
+              ></div>
+            )}
+            {!avatar && (
+              <div
+                className={styles.avatar}
+                style={{ backgroundImage: `url(${AvatarPlaceholder})` }}
+              ></div>
+            )}
+          </div>
+          {avatar && <Button className={styles.button}>Save</Button>}
         </div>
         <div className={styles.section}>
           <h4 className={styles.title}>Change Password</h4>
+          <div>
+            <input
+              type="text"
+              onChange={handlePasswordChange}
+              value={password}
+              ref={passwordRef}
+            />
+          </div>
+          {password?.length > 0 && (
+            <Button className={styles.button}>Save</Button>
+          )}
         </div>
-        <div className={styles.section}>
+        {/* <div className={styles.section}>
           <h4 className={styles.title}>Privacy</h4>
-        </div>
+          <div>
+            <input type="radio"/>
+          </div>
+          <Button className={styles.button}>Save</Button>
+        </div> */}
         <div className={styles.section}>
-          <h4 className={styles.title}>Delete Account</h4>
+          <h4 className={styles.title}>Delete User Account</h4>
+          <Button className={clsx(styles.button, "secondary")}>
+            Delete Account
+          </Button>
         </div>
       </div>
     </>
